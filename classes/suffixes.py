6334cb0183c1df813877f51390f4be9756ff4631
Oktay <oktayelb@gmail.com>
2025-03-26 02:09:54 +0300

    New classes and additional structure, still no set paradigm

--- classes/suffixes.py
+++ classes/suffixes.py
@@ -1,38 +1,80 @@
 from enum import Enum
-
+import word_methods as wrd
 class WType(Enum):
     Noun = 0
     Verb = 1
 
+class HasMajorHarmony(Enum):     ## I have to change this naming 
+    Yes = 0                   ## 1- its ugly
+    No = 1                 ## second the word_methods already has the samenamed enums
+
+class HasMinorHarmony(Enum):
+    Yes = 0
+    No = 1
+
 class Suffix():
-    def __init__(self,suffix,comes_to, makes):
+    def __init__(self,suffix,comes_to, makes,major_harmony,minor_harmony):
         self.suffix = suffix
         self.comes_to = comes_to
         self.makes = makes
+        self.major_harmony = major_harmony
+        self.minor_harmony = minor_harmony
+
+    def form(self,word):
+        if(self.major_harmony == HasMajorHarmony.Yes):
+            word_harmony =wrd.major_harmony(word)
+            if( word_harmony== wrd.MajorHarmony.Back): pass
+                ## if vowels in suffix are already in wrd.back_vowels do nothing
+                ## else swap the vowel with its conjugate
+            if(word_harmony == wrd.MajorHarmony.Front): pass
+                ## if vowels in suffix are already in wrd.front_vowels do nothing
+                ## else swap the vowel with its conjugate
+
+        if(self.minor_harmony == HasMinorHarmony.Yes): ## Buradaki bilgilerin dogrulugu supheli
+            word_harmony = wrd.minor_harmony(word)
+
+            if( word_harmony  == wrd.MinorHarmony.BackRound  ): pass
+                ## if vowels in suffix are already in ['u'] do nothing
+                ## else swap the vowel with 'u'
+            if( word_harmony  == wrd.MinorHarmony.BackWide   ): pass
+                ## if vowels in suffix are already in ['u'] do nothing
+                ## else swap the vowel with 'u'
+            if( word_harmony  == wrd.MinorHarmony.FrontRound ): pass
+                ## if vowels in suffix are already in ['ü'] do nothing
+                ## else swap the vowel with 'ü'
+            if( word_harmony  == wrd.MinorHarmony.FrontWide  ): pass
+                ## if vowels in suffix are already in ['e','i'] do nothing
+                ## else swap the vowel with 'u'
+
+        if(word[len(word)-1] in wrd.vowels and self.suffix[0] in wrd.vowels):pass  ## belk' bunu basha alip optimize edebilirsin
+            ## trim the leading vowel from the result 
+
+
 
      
-    
-reflexive_is = Suffix('iş', WType.Verb, WType.Verb)
-reflexive_ik = Suffix("ik", WType.Verb, WType.Verb)
-active_t     = Suffix("it", WType.Verb, WType.Verb)
-active_tir   = Suffix("tir",WType.Verb, WType.Verb)
-active_ir    = Suffix("ir", WType.Verb, WType.Verb)
-passive_il   = Suffix("il", WType.Verb, WType.Verb)
-passive_in   = Suffix("in", WType.Verb, WType.Verb)
+# V2V suffixes    
+reflexive_is = Suffix('iş', WType.Verb, WType.Verb, HasMajorHarmony.Yes, HasMinorHarmony.Yes)
+reflexive_ik = Suffix("ik", WType.Verb, WType.Verb, HasMajorHarmony.Yes, HasMinorHarmony.Yes)
+active_t     = Suffix("it", WType.Verb, WType.Verb, HasMajorHarmony.Yes, HasMinorHarmony.Yes)
+active_tir   = Suffix("tir",WType.Verb, WType.Verb, HasMajorHarmony.Yes, HasMinorHarmony.Yes)
+active_ir    = Suffix("ir", WType.Verb, WType.Verb, HasMajorHarmony.Yes, HasMinorHarmony.Yes)
+passive_il   = Suffix("il", WType.Verb, WType.Verb, HasMajorHarmony.Yes, HasMinorHarmony.Yes)
+passive_in   = Suffix("in", WType.Verb, WType.Verb, HasMajorHarmony.Yes, HasMinorHarmony.Yes)
 
 
-plural                =  Suffix("ler"   , WType.Noun, WType.Noun)
-actor                 =  Suffix("çi"    , WType.Noun, WType.Noun)
-dimunitive_cik        =  Suffix("çik"   , WType.Noun, WType.Noun)
-ordinal               =  Suffix("inci"  , WType.Noun, WType.Noun)
-approximative_si      =  Suffix("si"    , WType.Noun, WType.Noun)
-approximative_imsi    =  Suffix("imsi"  , WType.Noun, WType.Noun)
-approximative_imtrak  =  Suffix("imtrak", WType.Noun, WType.Noun)
-privative             =  Suffix("siz"   , WType.Noun, WType.Noun)
-counting_er           =  Suffix("er"    , WType.Noun, WType.Noun)
-cooperative_daş       =  Suffix("daş"   , WType.Noun, WType.Noun)
-philicative_cil       =  Suffix("cil"   , WType.Noun, WType.Noun)
-relative_ca           =  Suffix("ça"    , WType.Noun, WType.Noun)
-relative_sal          =  Suffix("sel"   , WType.Noun, WType.Noun)
-composessive_li       =  Suffix("li"    , WType.Noun, WType.Noun)
-suitative_lik         =  Suffix("lik"   , WType.Noun, WType.Noun)   
+#N2N suffixes
+plural                =  Suffix("ler"   , WType.Noun, WType.Noun, HasMajorHarmony.Yes, HasMinorHarmony.No)
+counting_er           =  Suffix("er"    , WType.Noun, WType.Noun, HasMajorHarmony.Yes, HasMinorHarmony.No)
+cooperative_daş       =  Suffix("daş"   , WType.Noun, WType.Noun, HasMajorHarmony.Yes, HasMinorHarmony.No)
+relative_ca           =  Suffix("ça"    , WType.Noun, WType.Noun, HasMajorHarmony.Yes, HasMinorHarmony.No)
+relative_sal          =  Suffix("sel"   , WType.Noun, WType.Noun, HasMajorHarmony.Yes, HasMinorHarmony.No)
+actor                 =  Suffix("çi"    , WType.Noun, WType.Noun, HasMajorHarmony.Yes, HasMinorHarmony.Yes)
+dimunitive_cik        =  Suffix("çik"   , WType.Noun, WType.Noun, HasMajorHarmony.Yes, HasMinorHarmony.Yes)
+ordinal               =  Suffix("inci"  , WType.Noun, WType.Noun, HasMajorHarmony.Yes, HasMinorHarmony.Yes)
+approximative_si      =  Suffix("si"    , WType.Noun, WType.Noun, HasMajorHarmony.Yes, HasMinorHarmony.Yes)
+approximative_imsi    =  Suffix("imsi"  , WType.Noun, WType.Noun, HasMajorHarmony.Yes, HasMinorHarmony.Yes)
+approximative_imtrak  =  Suffix("imtrak", WType.Noun, WType.Noun, HasMajorHarmony.Yes, HasMinorHarmony.Yes)
+privative             =  Suffix("siz"   , WType.Noun, WType.Noun, HasMajorHarmony.Yes, HasMinorHarmony.Yes)
+philicative_cil       =  Suffix("cil"   , WType.Noun, WType.Noun, HasMajorHarmony.Yes, HasMinorHarmony.Yes)
+composessive_li       =  Suffix("li"    , WType.Noun, WType.Noun, HasMajorHarmony.Yes, HasMinorHarmony.Yes)
+suitative_lik         =  Suffix("lik"   , WType.Noun, WType.Noun, HasMajorHarmony.Yes, HasMinorHarmony.Yes)   
--- word_methods.py
+++ word_methods.py
@@ -1,23 +1,30 @@
 from enum import Enum
 from suffixes import name2name
 
-vowels = ['a','o','u','ı','e','ö','ü','i',]
+vowels = ['a','o','u','ı','e','ö','ü','i',]     ##T.Turkcesindeki tum Unluler
 
-back_vowels = ['a','ı','o','u']
+back_vowels = ['a','ı','o','u']                 ## Kalin Unluler
 
-front_vowels =['e','i','ö','ü']
+front_vowels =['e','i','ö','ü']                 ## Ince Unluler
 
-fistikci_sahap = ['f','s','t','ç','ş','h','p']
-
-
-class Harmony(Enum):
-    Back  = 0
-    Front = 1
+fistikci_sahap = ['f','s','t','ç','ş','h','p']  ## Ikonik unsuz sertlestirmesine sokan unsuz harfler
 
 
+class MajorHarmony(Enum):   ## Buyuk Unlu Uyuymu
+    Back  = 0  ##Kalin
+    Front = 1  ##ince 
+
+class MinorHarmony(Enum):   ## Kucuk Unlu uyumu
+
+    BackRound   = 0  ##Kalin Yuvarlak 
+    BackWide    = 1  ##Kalin Duz
+    FrontRound  = 2  ##Ince Yuvarlak 
+    FrontWide   = 3  ##Ince Duz
 
 
-def exists(word):
+
+
+def exists(word):           ## Sozcuk words.txt te var mi onu kontrol ediyor.
     
     with  open("words.txt", "r", encoding="utf-8") as f:
         word_list = [line.strip() for line in f]
@@ -39,22 +46,38 @@ def exists(word):
 
 
 
-def harmony(word):
+def major_harmony(word):    ## Sozcuge gelecek ekler icin son unlusune bakiyor. Istisnalari saymiyor.
     i = len(word) -1
     while(word[i] not in vowels):
         i = i-1
     ## kuraldışıları da çözüver
     if (word[i] in back_vowels):
-        return Harmony.Back
+        return MajorHarmony.Back
     if (word[i] in front_vowels):
-        return Harmony.Front
+        return MajorHarmony.Front
     
 
-def infinitive(word):
-    return word + ("mak" if harmony(word) == Harmony.Back else "mek")
+def minor_harmony(word):
+    i = len(word) -1
+    while(word[i] not in vowels):
+        i = i-1
+    ## kuraldışıları da çözüver
+    temp = word[i]
+    if (temp in ['o','u']):
+        return MinorHarmony.BackRound
+    if (temp in ['ı','a']):
+        return MinorHarmony.BackWide
+    if (temp in ['ö','ü'] ):
+        return MinorHarmony.FrontRound
+    if (temp in ['i','e']):
+        return MinorHarmony.FrontWide
+
+
+def infinitive(word):       ## Sozcugun Mastarini veriyor  
+    return word + ("mak" if major_harmony(word) == MajorHarmony.Back else "mek")
  
 
-def isVerb(word):
+def isVerb(word):           ## girdinin sonuna mastar ekleyip var mi diye bakiyor. 
     return exists(infinitive(word))
 
     
